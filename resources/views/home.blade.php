57c727160b866c236c26a37defcc214488b1f3ba
sabeeha <sabeehakpm@gmail.com>
2021-09-25 18:26:05 +0300

    changes to dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,7 +36,7 @@
             <div class="icon">
                 <i class="ion ion-person-stalker"></i>
             </div>
-            <a href="{{ route('sales.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="{{ route('sales1.index') }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
