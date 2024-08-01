2e27ba3277e8ab96f3d77d8d427afbb25c51b026
dawnStreaks <sabeehakpm@gmail.com>
2024-08-01 14:18:37 +0300

    changes to 1 invoice

--- resources/views/sales1/productOutPDF.blade.php
+++ resources/views/sales1/productOutPDF.blade.php
@@ -194,7 +194,7 @@
             </tr>
         </table>
         <div>
-        <img src="{{ asset('upload/logo/qr-code.png') }}" >
+        <img src="{{ asset('upload/logo/qr-code.png') }}" style="width:50%; height:50%;">
 </div>
         <div>
 
